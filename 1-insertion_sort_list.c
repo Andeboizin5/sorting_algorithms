9506d9536bf2301d5df0619c2e3b1d1a9e18ea4c
Andeboizin5 <andeboizin5@gmail.com>
2024-02-20 05:10:38 -0800

    0-bubble_sort.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,36 +1,39 @@
 #include "sort.h"
 
-/**
+/*
  * insertion_sort_list - sorts a doubly linked list of integers
- * @list: lists input
+ * @list: list input
  * Return: Always 0
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current, *prev;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *curr = (*list)->next;
+	current = (*list)->next;
 
-	while (curr != NULL)
+	while (current)
 	{
-		listint_t *temp = curr->prev;
-
-		while (temp != NULL && temp->n > curr->n)
+		prev = current->prev;
+		while (prev && prev->n > current->n)
 		{
-			if (curr->next != NULL)
-				curr->next->prev = temp;
-			temp->next = curr->next;
-			curr->next = temp;
-			curr->prev = temp->prev;
-			temp->prev = curr;
-			if (curr->prev != NULL)
-				curr->prev->next = curr;
+			prev->next = current->next;
+			if (current->next)
+				current->next->prev = prev;
+
+			current->next = prev;
+			current->prev = prev->prev;
+			prev->prev = current;
+
+			if (current->prev)
+				current->prev->next = current;
 			else
-				*list = curr;
-			temp = curr->prev;
+				*list = current;
 			print_list(*list);
+			prev = current->prev;
 		}
-		curr = curr->next;
+		current = current->next;
 	}
 }
